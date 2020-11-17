6669b6909eaa5381b3c6d34cdbc06365f360fb35
Irina Ohara <irina.v.ohara@gmail.com>
2020-11-17 15:23:00 -0700

    Created initial files for cards view

---
+++ app/src/main/java/com/example/easymenuplanner/Card.java
@@ -0,0 +1,45 @@
+package com.example.easymenuplanner;
+
+public class Card {
+    String date, description, tag;
+    int image;
+
+    public Card(String date, String description, String tag, int image) {
+        this.date = date;
+        this.description = description;
+        this.tag = tag;
+        this.image = image;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getTag() {
+        return tag;
+    }
+
+    public void setTag(String tag) {
+        this.tag = tag;
+    }
+
+    public int getImage() {
+        return image;
+    }
+
+    public void setImage(int image) {
+        this.image = image;
+    }
+}
--- app/src/main/java/com/example/easymenuplanner/MainActivity.java
+++ app/src/main/java/com/example/easymenuplanner/MainActivity.java
@@ -1,14 +1,84 @@
 package com.example.easymenuplanner;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+
+    //actionbar
+    private ActionBar actionBar;
+
+    //UI views
+    private ViewPager viewPager;
+
+    private ArrayList<Card> cardArrayList;
+    private CardAdapter cardAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //initialize actionBar
+        actionBar = getSupportActionBar();
+
+        //init UI views
+        viewPager = findViewById(R.id.viewPager);
+        loadCards();
+        //set viewpager change listener
+        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+                String date = cardArrayList.get(position).getDate();
+                actionBar.setTitle(date);
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
+    }
+
+    private void loadCards() {
+        //init list
+        cardArrayList = new ArrayList<>();
+
+        //add items to list
+        cardArrayList.add(new Card(
+                "November 23, 2020",
+                "You have no recipes selected",
+                "Chicken",
+                R.drawable.monday
+        ));
+        cardArrayList.add(new Card(
+                "November 24, 2020",
+                "You have no recipes selected",
+                "Steak",
+                R.drawable.tuesday
+        ));
+        cardArrayList.add(new Card(
+                "November 25, 2020",
+                "You have no recipes selected",
+                "Fish",
+                R.drawable.wednesday
+        ));
+
+        //setup adapter
+        cardAdapter = new CardAdapter(this, cardArrayList);
+        //set adapter to view pager
+        viewPager.setAdapter(cardAdapter);
+        //set default padding
+        viewPager.setPadding(100, 0, 100, 0);
     }
 }
