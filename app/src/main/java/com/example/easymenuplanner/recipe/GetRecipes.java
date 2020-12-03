b405536d610b1795d2e60d78aa42ba6dc32789c4
dneves02 <71541305+dneves02@users.noreply.github.com>
2020-12-03 01:09:30 -0800

    GetRecipes

--- app/src/main/java/com/example/easymenuplanner/recipe/GetRecipes.java
+++ app/src/main/java/com/example/easymenuplanner/recipe/GetRecipes.java
@@ -1,4 +1,15 @@
 package com.example.easymenuplanner.recipe;
 
+import java.util.List;
+
 public class GetRecipes {
+    private String title;
+    private String url;
+    private String picture;
+    private List<String> ingredients;
+
+    public String getTitle() { return title; }
+    public String getUrl() { return url; }
+    public String getPicture() { return picture; }
+    public List<String> getIngredients() { return ingredients; }
 }
