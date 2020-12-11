64acae2323c420bf8e1024bed17d76364fcc6793
dneves02 <71541305+dneves02@users.noreply.github.com>
2020-12-10 20:13:52 -0800

    SearchApi Recipe Fragment ApiGroup, TopRecipes Type

--- app/src/main/java/com/example/easymenuplanner/searchRecipe/ApiRecipeGroup.java
+++ app/src/main/java/com/example/easymenuplanner/searchRecipe/ApiRecipeGroup.java
@@ -1,6 +1,14 @@
 package com.example.easymenuplanner.searchRecipe;
 
+import java.util.List;
+
 public class ApiRecipeGroup {
 
+    private List<ApiRecipeTop> hits;
 
+
+
+    public List<ApiRecipeTop> getAllRecipes() {
+        return this.hits;
+    }
 }
