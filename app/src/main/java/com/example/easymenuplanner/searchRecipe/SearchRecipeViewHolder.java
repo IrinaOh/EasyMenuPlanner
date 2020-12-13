e715b6dc19e344c2ab890c8d04dc4f17bf37d4c9
Thomas Weaver <tjayweaver@gmail.com>
2020-12-12 23:00:36 -0500

    api working but not images

--- app/src/main/java/com/example/easymenuplanner/searchRecipe/SearchRecipeViewHolder.java
+++ app/src/main/java/com/example/easymenuplanner/searchRecipe/SearchRecipeViewHolder.java
@@ -1,6 +1,9 @@
 package com.example.easymenuplanner.searchRecipe;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -8,17 +11,21 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.easymenuplanner.R;
 
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 
 public class SearchRecipeViewHolder extends RecyclerView.ViewHolder {
 
-    //private ImageView recipeImage;
+    private ImageView recipeImage;
     private TextView recipeLabel;
     private TextView recipeURL;
 
     public SearchRecipeViewHolder(@NonNull View itemView) {
         super(itemView);
 
-        //recipeImage = itemView.findViewById(R.id.ivRecipeImage);
+        recipeImage = itemView.findViewById(R.id.ivRecipeImage);
         recipeLabel = itemView.findViewById(R.id.tvRecipeLabel);
         recipeURL = itemView.findViewById(R.id.tvURL);
 
@@ -26,15 +33,17 @@ public class SearchRecipeViewHolder extends RecyclerView.ViewHolder {
 
     public void bindData(ApiRecipeTop recipeTop) {
         ApiRecipe recipe = recipeTop.getRecipe();
-//        try {
-//            URL imageURL = new URL(recipe.getImage());
-//            Bitmap bmp = BitmapFactory.decodeStream(imageURL.openConnection().getInputStream());
-//            recipeImage.setImageBitmap(bmp);
-//        } catch (MalformedURLException e) {
-//            e.printStackTrace();
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
+        try {
+            URL imageURL = new URL(recipe.getImage());
+            Bitmap bmp = BitmapFactory.decodeStream(imageURL.openConnection().getInputStream());
+            recipeImage.setImageBitmap(bmp);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
         recipeLabel.setText(recipe.getTitle());
         recipeURL.setText(recipe.getUrl());
